434a1a049736366c011800bfea72955fec22288d
georf1 <rh26157@gmail.com>
2023-07-21 16:29:48 +0300

    fix bugs

--- script.py
+++ script.py
@@ -14,32 +14,34 @@ def main():
     parser = argparse.ArgumentParser(description="Process video files and add music.")
     parser.add_argument("-f1", "--folder1", type=str, required=True, help="Path to folder 1.")
     parser.add_argument("-f2", "--folder2", type=str, required=True, help="Path to folder 2.")
-    parser.add_argument("-m", "--music", type=str, required=True, help="Path to music file.")
+    parser.add_argument("-m", "--music", type=str, required=True, help="Path to music folder.")
     args = parser.parse_args()
 
     try:
-        # Select first video
+        # Step 1
         video1_path = get_random_file(args.folder1)
         print("Selected video 1:", video1_path)
 
-        # Select second video
+        # Step 2
         video2_path = get_random_file(args.folder2)
         print("Selected video 2:", video2_path)
 
-        # Clip videos to 2 secs
-        video1 = mp.VideoFileClip(video1_path).subclip(0, 2)
-        video2 = mp.VideoFileClip(video2_path).subclip(0, 2)
+        # Step 3
+        video1 = mp.VideoFileClip(video1_path).subclip(0, 2).set_fps(30)
+        video2 = mp.VideoFileClip(video2_path).subclip(0, 2).set_fps(30)
 
-        # Select music
-        music = mp.AudioFileClip(args.music)
+        # Step 4
+        music_path = get_random_file(args.music)
+        print("Selected music:", music_path)
+        music = mp.AudioFileClip(music_path).subclip(0, 4)
 
         # Combine videos and add music
-        final_video = mp.concatenate_videoclips([video1, video2])
+        final_video = mp.concatenate_videoclips([video1, video2], method='compose')
         final_video = final_video.set_audio(music)
 
-        # Save output
+        # Step 5
         output_path = "output.mp4"
-        final_video.write_videofile(output_path, codec="libx264", audio_codec="aac")
+        final_video.write_videofile(output_path)
         print("The final video has been saved to:", output_path)
 
     except Exception as e:
